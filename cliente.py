c151140e68f6314f82b787647492698d7af74fc5
LERV1993 <lucas.emanuel_1993@hotmail.com>
2024-06-12 00:33:42 -0300

    Se finaliza sala de chat y se comenta todo el código utilizado

--- cliente.py
+++ cliente.py
@@ -1,51 +1,158 @@
 import sys
 import socket
+import threading
 
-TCP_IP = '127.0.0.1'
-PUERTO = 5500
-BUFFER_SIZE = 20
-MESSAGE = "Hola Server!"
+TCP_IP = '127.0.0.1'  # Para que quede en localhost y evitar ingresos o tipeados erróneos
+PUERTO = '' # Lo define el usuario por input
+BUFFER_SIZE = 200  # Aproximadamente 150 caracteres por si hay algún carácter especial que rompa todo en el programa lo limitamos a 150 pero le damos un margen extra
+NAME = '' # El nick visible
 
-# Ambos if referencian una entrada en la llamada al archivo pudiendo sobreescribir las constantes python cliente.py TCP_IP MENSSAGE
-if len(sys.argv) >= 2:
-    TCP_IP = sys.argv[1]
+# Ingreso manual de puerto
+while not PUERTO:
+    
+	# Mostramos por pantalla lo que debe realizar el usuario ....
+    print('[CONFIG] Para salir escriba "salir". \n')
+    print('[CONFIG] Ingrese el puerto que desea utilizar (entre 0 y 5500): \n')
+    input_user = input()
 
-if len(sys.argv) >= 3:
-    MESSAGE = sys.argv[2]
+	# Lower por si lo escribe sin querer con una mayúscula
+    # Cierra el programa y le avisamos por pantalla
+    if input_user.lower() == 'salir':
+        print('[CONFIG] Programa finalizado.')
+        sys.exit()
 
-
-print ("[CLIENTE] Iniciando")
-# Creamos el socket del cliente
-socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-print ("[CLIENTE] Conectando")
-# Creamos la conexión al server
-socket_cliente.connect((TCP_IP, PUERTO))
-
-print (f"[CLIENTE] soy el cliente: \"{str(socket_cliente.getsockname())}\"")
-print ("[CLIENTE] Enviando datos: \"" + MESSAGE + "\"")
-
-# Envío de datos al server
-socket_cliente.send((MESSAGE + '\n').encode('utf-8'))
-print ("[CLIENTE] Recibiendo datos del SERVIDOR")
-msg = ''
-fin_msg = False
-datos = bytearray()
-
-# Recibiendo datos del server
-while not fin_msg:
-	recvd = socket_cliente.recv(BUFFER_SIZE)
-	datos += recvd
-	print ("[CLIENTE] Recibidos ", len(recvd), " bytes")
-	if b'\n' in recvd:
-		msg = datos.rstrip(b'\n').decode('utf-8')
-		fin_msg = True
+	# Para evitar usar regex lo evaluamos pasandolo a int si hay un valor no numerico saldra en el ValueError y lo levantamos por ahí
+    try:
+        PUERTO = int(input_user)
+        
+        # Se deben respetar los valores detallados al inicios del programa en cuanto a rangos 0 a 5500.
+        if PUERTO < 0 or PUERTO > 5500:
+            print('[CONFIG] El valor ingresado no se encuentra en el rango esperado (0 - 5500).\n')
+            PUERTO = '' # Si ingresa un valor fuera de los rangos borramos el dato para que caiga de nuevo en el bucle
             
-print ("[CLIENTE] Recibidos en total ", len(datos), " bytes")
-print ("[CLIENTE] Dados recibidos en respuesta al CLIENTE: \"" + msg + "\"")
-print ("[CLIENTE] Cerrando conexion con el CLIENTE")
+    except ValueError:
+        print('[CONFIG] Por favor ingrese solo valores numéricos. \n')
+        PUERTO = '' # Si hubo un ingreso no correspondiente a los valores esperados de puerto también lo reinstanciamos a '' para que vuelva a caer en el bucle.
 
-# Cierre de socket del cliente
-socket_cliente.close()
+# Ingreso manual de nombre de usuario
+while not NAME:
+    
+	# Le damos las pautas al usuario para que termine haciendo lo que se le cante.
+    print('[CONFIG] Ingrese su nombre de usuario (entre 3 y 10 caracteres). \n')
+    nombre_ingresado = input()
+    
+	# Lower por si lo escribe sin querer con una mayúscula
+    # Acá también le dejamos salir del programa
+    if input_user.lower() == 'salir':
+        print('[CONFIG] Programa finalizado.')
+        sys.exit()
 
-print ("[CLIENTE] Finalizado.")
+	# Evaluamos si el largo del string ingresado está dentro de los rangos establecidos.
+    if 3 <= len(nombre_ingresado) <= 10:
+        NAME = nombre_ingresado # Si está todo ok asignamos el valor a la variable NAME ( NICK )
+        
+	# Si hizo todo mal no asignamos nada a la variable mostramos el error por pantalla y vuelve a iniciarse el bucle
+    else:
+        print('[CONFIG] Ha ingresado un nombre de usuario no válido. \n')
+
+# Todo ok la configuración del usuario resta iniciar la conexión 
+print(f"[{NAME}] Iniciando sala de chat. \n")
+
+try:
+    # Creamos el socket del cliente
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	# Un print fantasmagorico
+    print(f"[{NAME}] Conectando... \n")
+    
+    # Creamos la conexión al server
+    socket_cliente.connect((TCP_IP, PUERTO))
+    
+# Por si se cayo el server o ingresamos un dato mal
+except socket.error as msg:
+    
+    # Mostramos el numero y el detalle del error
+    print(f"Fallo en la conexión del socket. Código de Error: {msg.errno} , Mensaje de Error: {msg.strerror}")
+    sys.exit()
+
+# Otro de esos print que hacen creer que pasa algo
+print(f"[{NAME}] Iniciando sesión. \n")
+
+# Función para recibir mensajes
+def recibir_mensaje():
+    
+	# Bucle para recibir mensajes
+    while True:
+        
+		# Encerrado en except para detectar la perdida de conexión o la finalización de la misma por caso #LOGOUT
+        try:
+            # Recibir el mensaje que devuelve el server
+            mensaje = socket_cliente.recv(BUFFER_SIZE)
+            
+			# Si no hay mensaje genero una excepción de manera intencionada quiere decir que el mensaje que recibi esta incompleto o paso algo
+            if not mensaje:
+                raise ConnectionError
+            
+			# Mensaje en UTF-8 para respetar signos y acentos
+            print(mensaje.decode('utf-8'))
+        
+		# El error de conexión por caida del server o por #LOGOUT
+        except ConnectionError:
+            
+			# Muestra por pantalla que se ha cerrado la conexión cierra el socket y finaliza el programa para cerrar los hilos.
+            print(f"[SISTEMA] Se ha cerrado la conexión. \n")
+            print("[SISTEMA] Programa finalizado. \n")
+            socket_cliente.close()
+            sys.exit()
+
+# Función para enviar mensajes
+def enviar_mensaje():
+    
+	# Bucle para enviar mensajes
+    while True:
+        
+		# Lo encerramos en un try except por si se pierde la conexión
+        try:
+            
+			# Levantamos el mensaje y le agregamos el nick para no tener que tratarlo en el server que nos va a representar mas laburo *El futuro es hoy*
+            mensaje_usuario = input()
+            mensaje = f"[{NAME}] {mensaje_usuario}"
+
+			# Si el mensaje posee mas de 150 caracteres le decimos que no exceda ese valor y usamos continue para volver a caer en el bucle sin ejecutar lo que sigue
+            if len(mensaje) > 150:
+                
+                print(f"[SISTEMA] Por favor no exceda los 150 caracteres.")
+                continue
+            
+			# Si todo está ok enviamos el mensaje
+            socket_cliente.send(mensaje.encode('utf-8'))
+        
+		# La excepción levanta errores de envío posiblemente causados por caida del server
+        except:
+            
+			# Mostramos por pantalla que ha ocurrido un error, cerramos el socket y finalizamos el programa
+            print("[SISTEMA] Ha ocurrido un error en el envío del mensaje. \n")
+            print("[SISTEMA] Programa finalizado. \n")
+            socket_cliente.close()
+            sys.exit()
+
+# Configuramos los hilos en variables y los dejamos en daemon true asi si se termina el programa ellos tambien se terminan de ejecutar.
+hilo_enviar = threading.Thread(target=enviar_mensaje, daemon=True)
+hilo_recibir = threading.Thread(target=recibir_mensaje, daemon=True)
+
+# Inicio de las instancias de las funciones enviar y recibir en hilos
+hilo_enviar.start()
+hilo_recibir.start()
+
+# Para mantener el programa activo porque sino me tira [WinError 10054] supongo que es por el daemon true
+# y que al ser estructurado al finalizar da por sentado que debe finalizar la ejecución mas alla de que haya whiles dentro de los hilos 
+# Seguramente debí haber creado una función main y ejecutarla como dios manda
+try:
+    while True:
+        pass
+    
+# En caso de que apriete un ctrl + c para interrumpir la ejecución, cierra todo.
+except KeyboardInterrupt:
+    print("\n[SISTEMA] Programa finalizado por el usuario.")
+    socket_cliente.close()
+    sys.exit()
--- server.py
+++ server.py
@@ -1,71 +1,140 @@
-import threading
 import socket
+import threading
+import sys
 
-TCP_IP = '127.0.0.1'  # Escucha solo conexiones locales
-PUERTO = 5500
-TAM_BUFFER = 20   # Para mayor velocidad de conexión
+# Definimos el tamaño del buffer y las variables globales
+TAM_BUFFER = 200  # Lo dejo en 200 para poder aceptar masomenos 150 caracteres
+TCP_IP = '127.0.0.1'  # Puedes cambiar la IP por la que necesites
+PUERTO = 5000 # Puerto donde funciona el server
+clientes = [] # Lista de cliente
 
+# Función para manejar cliente recibe el socket y la dirección de cliente
 def manejar_cliente(socket_cliente, direccion_cliente):
-    print(f"Conexión recibida desde {direccion_cliente}.\n")
 
-    datos = bytearray()
-    print(f"[Servidor] Esperando datos del cliente {direccion_cliente}.\n")
+    global clientes # Tengo que declararle en el scope la variable global porque es re mamasa python
+    datos = bytearray() # Array de bytes para evaluar la recepción de datos y decodificar el mesaje y reenviarlo, tambien lo use para depurar y ver que llegue info
 
+    # Lo encerramos en un try except por cualquier proble de conexión que pueda haber con el cliente o desconexión del mismo
     try:
-        mensaje = ""
+
+        # Instanciamos un bucle while para dejar recibiendo mensajes
         while True:
+
+            # Guardamos en una variable los datos recibidos 
             datos_recibidos = socket_cliente.recv(TAM_BUFFER)
 
+            # Levantamos cualquier problema con la recepción de datos y salimos del bucle con break
             if not datos_recibidos:
+
+                # Informamos por pantalla el error y la dirección del cliente al que corresponde
                 print(f"[SERVIDOR] Se ha perdido la conexión con el cliente {direccion_cliente}.\n")
                 break
-            
-            datos += datos_recibidos
-            print(f"[Servidor] Recibidos {len(datos_recibidos)} bytes del cliente {direccion_cliente}.\n")
-            if b'\n' in datos_recibidos:
-                mensaje = datos.rstrip(b'\n').decode('utf-8')
-                break
-        
-        # Unifico los prints en una función para mayor legibilidad
-        datos_recibidos_respuesta(direccion_cliente, mensaje, datos_recibidos)
-        socket_cliente.send(datos_recibidos)
-        print(f"[SERVIDOR] Respuesta enviada \"{mensaje}\" al cliente {direccion_cliente} \n")
-    
+
+            datos += datos_recibidos # Guardo los datos recibidos en el array mutable de bytes
+            #print(f"[SERVIDOR] Recibidos {len(datos_recibidos)} bytes del cliente {direccion_cliente}.\n")
+            # El print de arriba para verificar que lleguen bytes
+
+            # Evaluo que lleguen datos
+            if datos_recibidos:
+
+                # Decodifico el mensaje para salvar acentos y caracteres especiales
+                mensaje = datos.decode('utf-8')
+                datos = bytearray() # Depuración y para reiniciar el buffer para el próximo mensaje
+
+                #print(repr(mensaje), str(mensaje in "#LOGOUT"))  // Depuración estaba enviando el nick y estaba intentando evaluar mensaje == #LOGOUT
+
+                # Si dentro del mensaje llega un #LOGOUT cierra la conexión
+                if "#LOGOUT" in mensaje:
+
+                    # Informa por pantalla la dirección del  cliente que se desconectará y luego usa break para terminar el bucle
+                    print(f"[SERVIDOR] El cliente {direccion_cliente} se ha desconectado.\n")
+                    break
+
+                # Reenviar el mensaje a todos los clientes conectados
+                for cliente in clientes:
+
+                    # Reenvia a todos los clientes menos al que realiza el envío del mensaje
+                    if cliente != socket_cliente:
+                        
+                        # Utilizamos un try except para levantar cualquier posible desconexión de algún usuario en el momento del reenvío del mensaje
+                        try:
+                            cliente.send(mensaje.encode('utf-8') + b'\n')
+                        except Exception as e:
+                            # getpeername() para ver el ip y el puerto que esta fallando en caso de que no se reenvie algun mensaje
+                            print(f"[SERVIDOR] No se pudo enviar el mensaje a {cliente.getpeername()}: {e}")
+
+    # Levanto las excepciones de conexión por cualquier interrupción que se pueda dar.
     except ConnectionError as error:
         print(f"[SERVIDOR] [CLIENTE] {direccion_cliente} | [ERROR] Socket error: {error}")
 
+    # Fuera del bucle cuando se ejecuta alguna excepción cierro el socket del cliente y lo elimino de la lista para no reenviarle mensajes.
     finally:
+        # Cierro el socket del cliente
         socket_cliente.close()
 
-def datos_recibidos_respuesta(direccion_cliente, mensaje, datos_recibidos):
-    print(f"[Servidor] Recibidos en total {len(datos_recibidos)} bytes del cliente {direccion_cliente}.\n")
-    print(f"[SERVIDOR] Datos recibidos del cliente con éxito: \"{mensaje}\". \n")
-    print(f"[Servidor] Enviando respuesta al cliente {direccion_cliente}.\n")
+        # Borro de la lista de clientes el socket cerrado
+        if socket_cliente in clientes:
+            clientes.remove(socket_cliente)
 
+        # Muestro por pantalla las acciones realizadas.
+        print(f"[SERVIDOR] Cliente {direccion_cliente} desconectado y eliminado de la lista.\n")
+
+# Función principal
 def main():
-    print("[SERVIDOR] iniciando")
-    print(f"[SERVIDOR] abriendo socket {str(PUERTO)} y escuchando en {TCP_IP}.\n")
 
+    global clientes # Tengo que declarar nuevamente la varialbe cliente como global sino no me deja acceder 
+
+    # Muestro por pantalla la información del IP y el puerto donde inicia la sala de chat
+    print("[SALA] Iniciando")
+    print(f"[SALA] Abriendo socket en el puerto {PUERTO} y escuchando en {TCP_IP}.\n")
+
+    # Creo el socket declarando la familia y el tipo de conexión
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((TCP_IP, PUERTO))
-    server_socket.listen(5) # 5 conexiones como máximo
 
-    print(f"[SERVIDOR] Iniciando. Escuchando en {TCP_IP}:{PUERTO}")
+    # Encierro en un try except el bind por cualquier problema de puertos, es decir, que el puerto este ocupado.
+    try:
 
-    while 1:
-        print("[SERVIDOR] Esperando conexión...")
-        conn, addr = server_socket.accept()
+        # Hago el bind del socket con el ip y el puerto y lo dejo escuchando
+        server_socket.bind((TCP_IP, PUERTO))
+        server_socket.listen(10)  # 10 conexiones como máximo
 
-        thread = threading.Thread(target=manejar_cliente,
-                              args=[conn, addr],
-                              daemon=True)
-        thread.start()
-        print (F"[SERVIDOR] Conexion con el cliente realizada. Direccion de conexion: {addr}")
+        # Muestro por pantalla que la sala inicio
+        print(f"[SALA] Iniciando. Escuchando en {TCP_IP}:{PUERTO}")
 
-    print (f"[SERVIDOR] Cerrando socket {str(PUERTO)}")
-    server_socket.close()
-    print ("[SERVIDOR] Finalizando.")
+        # Instancio un bucle para esperar nueva conexiones
+        while True:
 
+            # Muestro por pantalla la espera de conexiones
+            print("[SALA] Esperando nueva conexión...")
 
+            # Cuando se acepta una conexión guardo en dos variables el socket y el addres del cliente
+            socket_cliente, addr = server_socket.accept()
+
+            # Guardo el soccket del cliente en la lista de clientes para poder reutilizarlos
+            clientes.append(socket_cliente)
+
+            # Creo el hilo con la función de manejar cliente en daemon true para que en caso de que finalice el programa estos se cierren
+            thread = threading.Thread(target=manejar_cliente, args=(socket_cliente, addr), daemon=True)
+
+            # Inicializo el hilo con el socket del cliente que se conecto
+            thread.start()
+
+            # Muestro por pantalla el exito de la conexión y el address del usuario
+            print(f"[SERVIDOR] Conexión con el cliente realizada. Dirección de conexión: {addr}")
+
+    # Si cuando intento hacer el bind el puerto no está disponible muestro por pantalla el error y cierro el programa
+    except socket.error as e:
+        print(f"[SERVIDOR] El puerto no se encuentra disponible: {e}\n")
+        sys.exit()
+
+    # Por último cierro el socket del server y muestro por pantalla el puerto
+    finally:
+        server_socket.close()
+        print(f"[SERVIDOR] Cerrando socket {PUERTO}")
+        print("[SERVIDOR] Finalizando.")
+
+# Se utiliza para verificar si el script está siendo ejecutado directamente.
+# Si la condición es verdadera, el código dentro de este bloque se ejecutará. 
+# Si el script está siendo importado, el código dentro de este bloque no se ejecutará.
 if __name__ == "__main__":
     main()
